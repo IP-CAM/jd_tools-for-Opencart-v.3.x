2918b6b0c74795adb9ed37990b83c28a8151e4d5
JD <zelenuj@gmail.com>
2020-10-28 15:43:09 +0200

    Початкові файли. Створення меню і збереження в конфігу

---
+++ install.php
@@ -0,0 +1,18 @@
+<?php
+$this->load->model('setting/setting');
+
+$jd_settings = $this->model_setting_setting->getSetting('jd_tools_main');
+if (empty($jd_settings)) {
+	$this->model_setting_setting->editSetting(
+		'jd_tools_main',
+		[
+			'jd_tools_main_menu_items' => [
+				[
+					'route'  => 'common/dashboard',
+					'name'  =>  'Немає елементів',
+					'children'  =>  [],
+				]
+			]
+		]
+	);
+}
