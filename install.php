a6419bd5eb9381afc6653ba7019e88f91b86d4ed
JD <zelenuj@gmail.com>
2020-11-10 10:39:59 +0200

    minifix

--- install.php
+++ install.php
@@ -6,13 +6,13 @@ if (empty($jd_settings)) {
 	$this->model_setting_setting->editSetting(
 		'jd_tools_main',
 		[
-			'jd_tools_main_menu_items' => [
-				[
+			'jd_tools_main_menu_items'  =>  [
+				'dashboard' => [
 					'route'  => 'common/dashboard',
-					'name'  =>  'Немає елементів',
+					'name'  =>  'Головна',
 					'children'  =>  [],
 				],
-				[
+				'prom_ua_import'    =>  [
 					'route' =>  'tool/prom_ua_import',
 					'name'  =>  'Імпорт ПромУА',
 					'children'  => [],
